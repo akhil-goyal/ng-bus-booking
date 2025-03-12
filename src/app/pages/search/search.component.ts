77fb5b624df49c63cdf101df085f44b1ab7d651e
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-03-12 18:33:37 -0400

    implement from and to selection dropdown

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../service/master.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  locations$: Observable<any[]> = new Observable<any[]>;
+  masterSrv = inject(MasterService);
 
+  ngOnInit(): void {
+    this.getAllLocations();
+  }
+
+  getAllLocations() {
+    this.locations$ = this.masterSrv.getLocations();
+  }
 }
--- src/app/service/master.service.ts
+++ src/app/service/master.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ export class MasterService {
 
   constructor(private http: HttpClient) {}
 
-  getLocations() {
-    return this.http.get(this.apiUrl + 'GetBusLocations');
+  getLocations(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl + 'GetBusLocations');
   }
 }
