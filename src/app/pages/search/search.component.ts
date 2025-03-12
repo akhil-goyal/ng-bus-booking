9df7f3795e3761b2462d1451de81f8f94d176d28
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-03-12 18:58:57 -0400

    implement dynamic bus search

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -2,16 +2,24 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MasterService } from '../../service/master.service';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
 export class SearchComponent implements OnInit {
-  locations$: Observable<any[]> = new Observable<any[]>;
+  locations$: Observable<any[]> = new Observable<any[]>();
   masterSrv = inject(MasterService);
+  busList: any[] = [];
+
+  searchObj: any = {
+    fromLocation: '',
+    toLocation: '',
+    travelDate: '',
+  };
 
   ngOnInit(): void {
     this.getAllLocations();
@@ -19,5 +27,14 @@ export class SearchComponent implements OnInit {
 
   getAllLocations() {
     this.locations$ = this.masterSrv.getLocations();
+  }
+
+  onSearch() {
+    const {fromLocation, toLocation, travelDate} = this.searchObj;
+    this.masterSrv
+      .searchBus(fromLocation, toLocation, travelDate)
+      .subscribe((res: any) => {
+        this.busList = res;
+      });
   }
 }
